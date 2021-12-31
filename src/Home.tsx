fc8cd7637a3dd9c48485fd50c6f2556a13b1c6fa
gabus <gabusdev@outlook.com>
2021-12-31 18:28:16 -0500

    Se aprendio a trabajar con el state de React

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,25 @@
-import { FunctionComponent, MouseEvent } from "react";
+import { FunctionComponent, useState } from "react";
 
 interface HomeProps {}
 
-const handleClick = () => {
-  console.log("Hello World");
-};
-
-const handleAgain = (name: string) => {
-  console.log(`Hello Mr.${name}`);
-};
-
-const handleEventTest = (t: string, e: any) => {
-  console.log(`El valor de t es ${t} y\nLa un dato es: ${e.timeStamp}`);
-  console.log(typeof e);
-};
-
 const Home: FunctionComponent<HomeProps> = () => {
+  // let name = "mario"; //Con esta definicion la variable se cambia pero no se refleja en el DOM
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    // name = "luigi";
+    setName("luigi");
+    setAge(30);
+  };
+
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => handleAgain("John")}>Click Again</button>
-      <button onClick={(e) => handleEventTest("test", e)}>Event Test</button>
     </div>
   );
 };
