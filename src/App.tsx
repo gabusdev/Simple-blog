be8f53b51e8fc058a307daa57cda78d51fb50329
gabus <gabusdev@outlook.com>
2021-12-31 15:30:15 -0500

    Primer test con un componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
+  const title = "Welcome to the new Blog";
+  const likes = 40;
+  const link = "https://www.gogle.com";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='content'>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{`2 al cuadrado = ${Math.pow(2, 2)}`}</p>
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
