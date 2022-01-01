fb2b49ee63aca2e0dca9edf2b6d34a7800b282b2
gabus <gabusdev@outlook.com>
2021-12-31 22:18:47 -0500

    Se reutilizan componentes y se modifica un poco el bloglist

--- src/BlogList.tsx
+++ src/BlogList.tsx
@@ -3,13 +3,15 @@ import Blog from "./Blog";
 
 interface BlogListProps {
   blogs: Array<any>;
+  title: string;
 }
 
-const BlogList: FunctionComponent<BlogListProps> = ({ blogs }) => {
+const BlogList: FunctionComponent<BlogListProps> = ({ blogs, title }) => {
   // const blogs = props.blogs; // Se puede usar asi llamando al parametro prop o como lo hice ahora
 
   return (
     <div className='blog-list'>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <Blog
           id={blog.id}
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,18 +6,23 @@ interface HomeProps {}
 const Home: FunctionComponent<HomeProps> = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new Website", body: "Lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "Lorem ipsum...", author: "luigi", id: 3 },
     {
-      title: "New Flavours 2022",
+      title: "New Flavours 2020", // Este se abre asi porq segun la extension Prettier esta linea es muy larga
       body: "Lorem ipsum...",
       author: "yoshi",
       id: 2,
     },
+    { title: "Welcome party!", body: "Lorem ipsum...", author: "luigi", id: 3 },
+    { title: "Best Blog Ever", body: "Lorem ipsum", author: "mario", id: 4 },
   ]);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} title='All Blogs!' />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's"
+      />
     </div>
   );
 };
