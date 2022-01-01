56b5a0fc691f04a4d16cc924b3c32ec3597a84e2
gabus <gabusdev@outlook.com>
2022-01-01 17:55:52 -0500

    Añadido gestion de errores a la hora de obtener datos

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,21 +6,37 @@ interface HomeProps {}
 
 const Home: FunctionComponent<HomeProps> = () => {
   const [blogs, setBlogs] = useState<BlogModel[]>([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
-    fetch("https://localhost:7275/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("https://localhost:7275/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch data from resource");
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError("");
+        })
+        .catch((err: Error) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
     // console.log("use effect ran");
   }, []);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All Blogs!' />
+      {isPending && <div>Loading...</div>}
+      {!isPending && <BlogList blogs={blogs} title='All Blogs!' />}
+      {error && <div>{error}</div>}
     </div>
   );
 };
