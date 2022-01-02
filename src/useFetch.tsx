06f8cdaf576c2f023ee28fc2ef1a7054436eea1e
gabus <gabusdev@outlook.com>
2022-01-02 18:28:55 -0500

    Se modifico el Readme y se retiro el tiempo de espera para cargar datos q estaba como prueba

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -7,29 +7,27 @@ export function useFetch<T>(url: string) {
 
   useEffect(() => {
     const abortCont = new AbortController();
-    setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could not fetch data from that resource");
-          }
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data from that resource");
+        }
 
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+        setError("");
+      })
+      .catch((err: Error) => {
+        if (err.name === "AbortError") {
+          console.log("Aborted");
+        } else {
+          setError(err.message);
           setIsPending(false);
-          setError("");
-        })
-        .catch((err: Error) => {
-          if (err.name === "AbortError") {
-            console.log("Aborted");
-          } else {
-            setError(err.message);
-            setIsPending(false);
-          }
-        });
-    }, 500);
+        }
+      });
     return () => abortCont.abort();
   }, [url]);
 
