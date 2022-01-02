54db301d8afb981986a2756e8acb2c7d2015d4ab
gabus <gabusdev@outlook.com>
2022-01-02 16:20:21 -0500

    Se logra utilizar la api para agregarun nuevo blog a la db

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,4 +1,6 @@
-import { FunctionComponent, useState } from "react";
+import { ChangeEvent, FunctionComponent, useState } from "react";
+import { BlogModel } from "./Types";
+import { useNavigate } from "react-router-dom";
 
 interface CreateProps {}
 
@@ -6,11 +8,31 @@ const Create: FunctionComponent<CreateProps> = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
+
+  const handelSubmint = (e: any) => {
+    e.preventDefault();
+    const newBlog = { title, body, author };
+
+    setIsPending(true);
+    // console.log(newBlog);
+    fetch("https://localhost:7275/blogs", {
+      method: "Post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newBlog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+      navigate("/");
+      // navigate(-1)
+    });
+  };
 
   return (
     <div className='create'>
       <h2>Create a new Blogpost</h2>
-      <form>
+      <form onSubmit={handelSubmint}>
         <label>Blog Title:</label>
         <input
           type='text'
