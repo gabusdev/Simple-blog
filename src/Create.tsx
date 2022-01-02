a6f8bbb6586e479ffc403c31c04a1cb6359c9079
gabus <gabusdev@outlook.com>
2022-01-02 15:31:07 -0500

    Se modifico el componente Create y se le dio estilo

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,9 +1,39 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 interface CreateProps {}
 
 const Create: FunctionComponent<CreateProps> = () => {
-  return <h2>Create a new Blogpost</h2>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
+  return (
+    <div className='create'>
+      <h2>Create a new Blogpost</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value='mario'>mario</option>
+          <option value='luigi'>luigi</option>
+          <option value='yoshi'>yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
